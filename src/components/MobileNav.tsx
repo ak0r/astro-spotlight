6ca2e80e71a7c3375d0cf45ed950e6ab145093ed
Amit K <androidak0r@gmail.com>
2024-01-20 02:11:25 +0530

    fix: :bug: Added navigation config file

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import { headerNav } from '../configs/navConfig.json'
 
 export default function App() {
   return (
@@ -10,12 +11,13 @@ export default function App() {
         </Button>
       </DropdownTrigger>
       <DropdownMenu aria-label="Static Actions">
-        <DropdownItem key="new">New file</DropdownItem>
-        <DropdownItem key="copy">Copy link</DropdownItem>
-        <DropdownItem key="edit">Edit file</DropdownItem>
-        <DropdownItem key="delete" className="text-danger" color="danger">
-          Delete file
-        </DropdownItem>
+        {
+          headerNav.map((page) => {
+            return (
+              <DropdownItem key={page.url}>{page.title}</DropdownItem>
+            );
+          })
+        }
       </DropdownMenu>
     </Dropdown>
   );
