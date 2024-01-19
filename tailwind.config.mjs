d22a035a974d2e06317cef313464577ae998123a
Amit K <androidak0r@gmail.com>
2024-01-19 12:38:38 +0530

    build: :heavy_plus_sign: Added NextUI
    Added NextUI and React to the project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const { nextui } = require("@nextui-org/react");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+	],
 	theme: {
-		extend: {},
+		extend: {
+
+		},
 	},
-	plugins: [],
+	plugins: [
+		nextui()
+	],
+	darkMode: 'class',
 }
