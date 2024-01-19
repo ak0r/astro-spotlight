91c4e8a54c321b0d4ad6769ea90e12ea5f352620
Amit K <androidak0r@gmail.com>
2024-01-19 15:31:31 +0530

    feat: :sparkles: Added colours
    Added primary and secondary colours to theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,7 +18,75 @@ export default {
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
-		nextui()
+		nextui({
+			themes: {
+			  light: {
+				colors: {
+				  background: "#FBFBFB",
+				  foreground: "#1B1B1E",
+				  primary: {
+					50: '#ffeedd',
+					100: '#ffd2b1',
+					200: '#fcb483',
+					300: '#f99753',
+					400: '#f67a23',
+					500: '#dc6009',
+					600: '#ac4b05',
+					700: '#7b3502',
+					800: '#4b1f00',
+					900: '#1e0800',
+					DEFAULT: "#F56E0F"
+				  },
+				  secondary: {
+					50: '#e0f8ff',
+					100: '#bee6f2',
+					200: '#99d4e5',
+					300: '#73c3d9',
+					400: '#4fb3cd',
+					500: '#3699b3',
+					600: '#26778c',
+					700: '#175565',
+					800: '#04343e',
+					900: '#001319',
+					DEFAULT: '#247084'
+				  }
+				},
+			  },
+			  dark: {
+				colors: {
+				  background: "#151419",
+				  foreground: "#D4D4D6",
+				  primary: {
+					50: '#ffeedd',
+					100: '#ffd2b1',
+					200: '#fcb483',
+					300: '#f99753',
+					400: '#f67a23',
+					500: '#dc6009',
+					600: '#ac4b05',
+					700: '#7b3502',
+					800: '#4b1f00',
+					900: '#1e0800',
+					DEFAULT: "#F56E0F"
+				  },
+				  secondary: {
+					50: '#e0f8ff',
+					100: '#bee6f2',
+					200: '#99d4e5',
+					300: '#73c3d9',
+					400: '#4fb3cd',
+					500: '#3699b3',
+					600: '#26778c',
+					700: '#175565',
+					800: '#04343e',
+					900: '#001319',
+					DEFAULT: '#247084'
+				  }
+				},
+			  }
+			}
+		  }
+		)
 	],
 	darkMode: 'class',
 }
