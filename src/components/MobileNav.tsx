d318de5bd0b669566a3e40a7833211f9167f8f48
Amit K <androidak0r@gmail.com>
2024-01-20 14:47:54 +0530

    fix: :bug: Fixed layout issues in header

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <Dropdown backdrop="blur">
       <DropdownTrigger>
-        <Button variant="ghost" radius="full" color="default">
+        <Button variant="ghost" radius="full" color="default" className="backdrop-blur dark:bg-zinc-800 bg-white/90 text-zinc-800 dark:text-zinc-200 text-sm font-medium">
           Menu
         </Button>
       </DropdownTrigger>
