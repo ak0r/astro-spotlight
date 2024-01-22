a7068e4fd3c58c3e41d0edffbe709f217ccd45b6
Amit K <androidak0r@gmail.com>
2024-01-22 10:04:35 +0530

    build: :heavy_plus_sign: Added vercel depenndency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()]
+  integrations: [tailwind(), react(), icon()],
+  output: "server",
+  adapter: vercel()
 });
