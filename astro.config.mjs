3c3320b1897ad53f332a6706df5fc7fb2d74e5ce
Amit K <androidak0r@gmail.com>
2024-01-22 14:34:00 +0530

    fix: :bug: Fixed rendering issue buy removing Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon()],
-  output: "server",
-  adapter: vercel()
+  output: "hybrid",
 });
