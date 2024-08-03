3d315d4d9a6d0cb6f6be85855e9d8efba6d13adc
Amit K <androidak0r@gmail.com>
2024-08-03 18:23:22 +0530

    feat: Added search functionality
    Added search functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), pagefind()],
   site: "https://astro-spotlight.pages.dev",
   domains: ["picsum.photos"],
+  build: {
+    format: "file",
+  },
 });
